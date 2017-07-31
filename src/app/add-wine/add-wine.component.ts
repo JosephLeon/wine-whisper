d36ae0fa56ca3da8f419150ad83a16e5f9a85532
Joseph Leon <joseph.leon.9@gmail.com>
2017-07-30 20:38:03 -0400

    Update form for add wine.

--- src/app/add-wine/add-wine.component.ts
+++ src/app/add-wine/add-wine.component.ts
@@ -9,6 +9,17 @@ export class AddWineComponent implements OnInit {
 
   constructor() { }
 
+  clickMessage = '';
+  onClickMe() {
+    this.clickMessage = 'You are my hero!';
+    console.log(this.clickMessage);
+  }
+
+  values = '';
+  onKey(event: any) { // without type info
+    this.values += event.target.value + ' | ';
+  }
+
   ngOnInit() {
   }
 
